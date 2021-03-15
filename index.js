d399d3cca892d403f59e3fd82d61efb0be9fffb9
Carlos Andrés Afanador Cabal <carlosafanador97@gmail.com>
2021-03-15 11:45:48 -0500

    Updated cors

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const cors = require('cors');
-
 const router = require('./utils/router');
+
+app.use(cors());
+app.use(router);
+
 const options = require('./socket/options');
 const { onStart } = require('./socket/handlers');
 const io = require('socket.io')(server, options);
 
-app.use(cors());
-app.use(router);
 
 onStart(io);
 
