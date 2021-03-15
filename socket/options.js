724bc0689c47e79b1baa3cd56d1dc8a81060b22c
Carlos Andrés Afanador Cabal <carlosafanador97@gmail.com>
2021-03-15 12:20:14 -0500

    Updated cors

--- socket/options.js
+++ socket/options.js
@@ -1,6 +1,6 @@
 const corsOptions = {
     cors: {
-        origin: "https://604f9624484b7829b5c35abd--elegant-hamilton-646fc3.netlify.app/",
+        origin: "https://604f9624484b7829b5c35abd--elegant-hamilton-646fc3.netlify.app",
         methods: ["GET", "POST"],
     }
 };
